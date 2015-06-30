d1c3545180016fbc0787534d4d456a006c61c1c8
andyrytina <andy.rytina@gmail.com>
2015-06-30 06:55:04 +0200

    added a waiting routine for the callback in the tests

--- com.socialapp.services.test/src/com/socialapp/services/test/SocialappServicesTest.java
+++ com.socialapp.services.test/src/com/socialapp/services/test/SocialappServicesTest.java
@@ -14,6 +14,7 @@ import com.socialapp.services.internal.callback.custom.ProcessableCallback;
 public class SocialappServicesTest {
 
 	protected String response;
+	protected AjaxStatus status;
 	
 	@Before
 	public void setup(){
@@ -26,7 +27,7 @@ public class SocialappServicesTest {
 	}
 
 	@Test
-	public void testResponseIsNotNull() throws InterruptedException{
+	public void testResponseIsNotNull() {
 		
 		
 		String url = "http://www.google.com";
@@ -35,6 +36,7 @@ public class SocialappServicesTest {
         aq.ajax(url, new ProcessableCallback<String>(null) {
 
             public void callback(String url, String resp, AjaxStatus status) {
+            	SocialappServicesTest.this.status = status;
             	SocialappServicesTest.this.response = resp;
             }
 
@@ -49,8 +51,21 @@ public class SocialappServicesTest {
 			}
         });
 		
-        Thread.sleep(1000);
+        waitForCallback();
         assertNotNull("the response must not be null!", response);
+	}
+
+	private void waitForCallback() {
+		for (int i = 0; i < 10; i++) { // wait at most 10 seconds
+			if(this.status != null){
+				return;
+			}
+			try{
+				Thread.sleep(1000);
+			}catch(InterruptedException ex){
+				// ignore
+			}
+		}
 	}
 
 }
